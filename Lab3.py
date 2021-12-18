264f0a98cedf6f64cc5ed907a2488398d7d5a1d2
RaSSlabowni <90353053+RaSSlabowni@users.noreply.github.com>
2021-12-19 04:08:49 +0500

    Диалоговый режим с пользователем

--- Lab3.py
+++ Lab3.py
@@ -1,9 +1,9 @@
-alfavitRU = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
-alfavitEU = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-yazik = input('Введите язык алфавита RU/EU: ')
-soobshenie = input('Введите сообщение для шивровки: ').upper()
-shag = int(input('Введите шаг шифровки: '))
-shifr = ''
+alfavitRU = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ' #Русский алфавит
+alfavitEU = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' #Английский алфавит 
+yazik = input('Введите язык алфавита RU/EU: ') #Выбор алфавита
+soobshenie = input('Введите сообщение для шивровки: ').upper() #Ввод сообщения для шифрования
+shag = int(input('Введите шаг шифровки: ')) #Ввод шага
+shifr = '' #Строка для вывода шифра
 if yazik == 'RU':
     for i in soobshenie:
         a = alfavitRU.find(i)   
@@ -18,8 +18,8 @@ elif yazik == 'EU':
             shifr += alfavitEU[(a + shag)%26]
         else:
             shifr += i
-print (shifr)
-print ('Если хотите расшифровать сообщение нажмите 1')
-vibor = int(input())
+print (shifr) #Вывод зашифрованного сообщение
+print ('Если хотите расшифровать сообщение нажмите 1') 
+vibor = int(input()) #Дешифровка
 if vibor == 1:
     print (soobshenie)
